2af2e05df4a5863681ac25184a46c1b93970d20d
anya.shan <anya.shanaida@yahoo.com>
2022-08-24 18:48:03 +0300

    counter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import debounce from 'lodash.debounce';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
@@ -24,39 +25,55 @@ function fetchCountries(name) {
     .then(respons => {
       return respons.json();
     })
-    .then(console.log);
+    .then(markup);
 }
 
-// function markupListCountriesEl(countries) {
-//   return countries
-//     .map(
-//       ({ name: { official }, flags: { svg } }) => `<li class="country-item">
-//       <img src="${svg}" alt="flag"/>
-//       ${official}
-//     </li>`
-//     )
-//     .join('');
+function markup(allCountries) {
+  let amountOfCountries = allCountries.length;
+
+  if (amountOfCountries > 10) {
+    return alert('Too many matches found. Please enter a more specific name.');
+  }
+
+  if (amountOfCountries >= 2 && amountOfCountries <= 10) {
+    return markupListCountriesEl(countries);
+  }
+}
+
+// function noMarkupEl() {
+//   alert('Too many matches found. Please enter a more specific name.');
 // }
-// listCountriesEl.insertAdjacentHTML('beforeend', markupListCountriesEl);
 
-function markupItemCountryEl({
-  name: { official },
-  capital,
-  population,
-  flags: { svg },
-  languages,
-}) {
-  const listOFLanguages = Object.values(languages);
-
-  return `<div class="country-info">
-            <h2 class="country-title">
-                <img src="${svg}" alt="flag" class="country-img"/>
-                ${official}
-            </h2>
-            <p class="country-text">Capital: ${capital}</p>
-            <p class="country-text">Population: ${population}</p>
-            <p class="country-text">Languages: ${listOFLanguages}</p>
-        </div>`;
+function markupListCountriesEl(countries) {
+  return countries
+    .map(
+      ({ name: { official }, flags: { svg } }) => `<li class="country-item">
+      <img src="${svg}" alt="flag"/>
+      ${official}
+    </li>`
+    )
+    .join('');
 }
+// refs.listCountriesEl.insertAdjacentHTML('beforeend', markupListCountriesEl);
 
-// infoOfCountryEl.insertAdjacentHTML('beforeend', markupItemCountryEl);
+// function markupItemCountryEl({
+//   name: { official },
+//   capital,
+//   population,
+//   flags: { svg },
+//   languages,
+// }) {
+//   const listOFLanguages = Object.values(languages);
+
+//   return `<div class="country-info">
+//             <h2 class="country-title">
+//                 <img src="${svg}" alt="flag" class="country-img"/>
+//                 ${official}
+//             </h2>
+//             <p class="country-text">Capital: ${capital}</p>
+//             <p class="country-text">Population: ${population}</p>
+//             <p class="country-text">Languages: ${listOFLanguages}</p>
+//         </div>`;
+// }
+
+// refs.infoOfCountryEl.insertAdjacentHTML('beforeend', markupItemCountryEl);
