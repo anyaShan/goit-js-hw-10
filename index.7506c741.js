18a8080d0069aa701e6c32c23f657d0f9a2bd38b
anyaShan <99140476+anyaShan@users.noreply.github.com>
2022-08-24 17:01:13 +0000

    Deploying to gh-pages from @ anyaShan/goit-js-hw-10@179d3743fb9200d9f1b91b8cd9d5eaa3d17eff18 🚀

--- index.7506c741.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=fetch("https://restcountries.com/v3.1/name/peru");console.log(e)}();
-//# sourceMappingURL=index.7506c741.js.map
--- index.cf8d9f3c.js
+++
@@ -1,2 +0,0 @@
-const e=fetch("https://restcountries.com/v3.1/name/peru");console.log(e);
-//# sourceMappingURL=index.cf8d9f3c.js.map
